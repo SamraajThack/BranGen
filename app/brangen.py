1209edfb15f641a68987e6b3a29ae837af0dc66c
Samraaj Thackeray <samraajthackeray@gmail.com>
2022-05-14 08:24:56 -0400

    Implemeted generate_snippet, returns formatted branding snippet from 3rd party api cll

--- app/brangen.py
+++ app/brangen.py
@@ -1,16 +1,42 @@
 import os
 import openai
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-subject  = "coffee"
-prompt = f"Generate upbeat branding snippet for{subject}"
+import argparse
 
 
-response = openai.Completion.create(
-    engine="text-davinci-001", prompt = prompt , max_tokens=32
-)
+def main():
+    print("Running BranGen")
 
-print(response)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
+
+    generate_branding_snippet(user_input)
+    pass
 
 
+def generate_branding_snippet(prompt: str):
+
+    # Load API key from env
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    instruct_prompt = f"Generate upbeat branding snippet for {prompt} "
+
+    # get the snippet from the the 3rd party API by providing instruction
+    response = openai.Completion.create(
+        engine="text-davinci-001", prompt=instruct_prompt, max_tokens=32
+    )
+
+    # extract text from reponse
+    branding_text: str = response["choices"][0]["text"]
+    #strip whitespce
+    branding_text = branding_text.strip() 
+
+    # add th ellipses if the sentence is not complete
+    last_char = branding_text[-1]
+    if last_char not in {".", "!", "?"}:
+        branding_text += "..."
+    print(branding_text)
+
+
+if __name__ == "__main__":
+    main()
