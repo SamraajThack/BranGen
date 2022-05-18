8ff370fdb9a4f9f1fc210f53ee1e3854483a4736
Samraaj Thackeray <samraajthackeray@gmail.com>
2022-05-18 19:24:01 -0400

    Completed AWS lambda function with docker image as a base layer

--- brangen_infra/lib/brangen_infra-stack.ts
+++ brangen_infra/lib/brangen_infra-stack.ts
@@ -1,17 +1,33 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from "dotenv";
+import { DEFAULT_ACCOUNT_ENV } from 'aws-cdk-lib/cx-api';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+
+dotenv.config()
 
 export class BrangenInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const apiLambda = new lambda.Function(this, "ApiFunction", {
-      runtime: lambda.Runtime.PYTHON_3_9,
-      code: lambda.Code.fromAsset("../app/"),
-      handler: "brangen_api.handler"
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes:[
+        lambda.Runtime.PYTHON_3_7
+      ],
+    });
 
+    const apiLambda = new lambda.Function(this, "ApiFunction", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      code: lambda.Code.fromAsset("../app/"),
+      handler: "brangen_api.handler",
+      layers : [layer],
+      environment:{
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+      }
     });
   }
 }
